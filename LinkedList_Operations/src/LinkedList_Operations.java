de4fd9232bd0faea63b83905550fd8b8e2b88c93
Bhoomika-NV <bhoomikanv6@gmail.com>
2025-02-13 00:42:18 +0530

    finding length, searching and etc..

--- LinkedList_Operations/src/LinkedList_Operations.java
+++ LinkedList_Operations/src/LinkedList_Operations.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,8 +11,37 @@ public class LinkedList_Operations {
 		print();// printing
 		delete();// deleting the items
 		update();// updates
-		// reversing();
+	   reversing();
+	   reversingInternalLogic();
+	   containsElement();
 	}
+
+	private static void containsElement() {
+	     List<Integer> list = new LinkedList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,1));
+	      boolean flag= list.contains(9);
+	      System.out.println(flag);
+	}
+
+	private static void reversingInternalLogic() {
+     List<Integer> list = new LinkedList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,1));
+     int start=0;
+     int end=list.size()-1;
+     while(start < end) {
+    	 int temp=list.get(start);
+    	 list.set(start,list.get(end));
+    	 list.set(end,temp); 
+    	 start++;
+    	 end--;
+     }
+     System.out.println("reversed = "+ list);
+	}
+
+	private static void reversing() {
+		List<Integer> linkedlist = new LinkedList<>(Arrays.asList(2, 4, 6, 8, 10, 12));
+	 //   ListIterator<Integer> iterator = linkedlist.listIterator();
+	    Collections.reverse(linkedlist);
+	    System.out.println("reversed list "+linkedlist);
+ 	}
 
 	private static void update() {
 		List<Integer> linkedlist = new LinkedList<>(Arrays.asList(2, 4, 6, 8, 10, 12));
@@ -19,14 +49,14 @@ public class LinkedList_Operations {
     for (int i = 0; i < linkedlist.size(); i++) {
          linkedlist.set(i, num++);
      }
-*/    System.out.println(linkedlist);
+*/    
 		ListIterator<Integer> listIterator = linkedlist.listIterator();
 		int num = 1; // Start numbering from 1
 		while (listIterator.hasNext()) {
 			listIterator.next(); // Move to the next element
 			listIterator.set(num++); // Set the current element and increment num
 		}
-		System.out.println(linkedlist);
+		System.out.println("updated list"+linkedlist);
 
 	}
 
@@ -38,13 +68,14 @@ public class LinkedList_Operations {
 				iterator.remove();
 			}
 		}
-		System.out.print("After deleting" + linkedlist + " ");
+		System.out.print("After deleting " + linkedlist + " ");
 		System.out.println();
 	}
 
 	private static void print() {
 		List<Integer> linkedlist = new LinkedList<Integer>(Arrays.asList(2, 4, 6, 8, 10, 12));
 		Iterator<Integer> i = linkedlist.iterator();
+		System.out.print("List is ");
 		while (i.hasNext()) {
 			System.out.print(i.next() + " ");
 		}
@@ -60,6 +91,5 @@ public class LinkedList_Operations {
 		linkedlist.add(10);
 		linkedlist.add(12);
 		System.out.println("after adding " + linkedlist + " ");
-
 	}
 }
